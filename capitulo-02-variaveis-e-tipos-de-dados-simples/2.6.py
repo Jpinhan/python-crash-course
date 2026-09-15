1db27cc9e7af970a7a0abbc7af721b63f0c3ab53
João <joaopinhan@gmail.com>
2026-09-15 07:36:18 -0300

    Adiciona exercicio 2.6 do capitulo 2

---
+++ capitulo-02-variaveis-e-tipos-de-dados-simples/2.6.py
@@ -0,0 +1,3 @@
+famous_person = "Padre Pio"
+message = famous_person + " uma vez disse: Reze, espere e não se preocupe. A preocupação é inútil. Nosso Senhor misericordioso escutará a sua oração. "
+print(message)
