28333e7558ffff429105dc7c7ecc4c58baf27cd3
João <joaopinhan@gmail.com>
2026-09-16 07:35:15 -0300

    Adiciona a pasta dos exercicios do capitulo 3

--- capitulo-02-variaveis-e-tipos-de-dados-simples/2.10.py
+++ capitulo-02-variaveis-e-tipos-de-dados-simples/2.10.py
@@ -1,3 +1,5 @@
+##o comando .strip() corta espaços em branco dos dois lado, o lstrip() corta do lado esquerdo e rstrip() corta do lado direito
+#o comando \n pula uma linha e o comando \t dá espaço de uma identação
 name = "\n\tjoao "
 print(name)
 print(name.strip())
