e80a9fbfcedfef7341960d68f4928a759311ee86
João <joaopinhan@gmail.com>
2026-09-16 07:43:05 -0300

    "Isso reverte um arquivo que foi comitado errado"
    This reverts commit 28333e7558ffff429105dc7c7ecc4c58baf27cd3.

--- capitulo-02-variaveis-e-tipos-de-dados-simples/2.10.py
+++ capitulo-02-variaveis-e-tipos-de-dados-simples/2.10.py
@@ -1,5 +1,3 @@
-##o comando .strip() corta espaços em branco dos dois lado, o lstrip() corta do lado esquerdo e rstrip() corta do lado direito
-#o comando \n pula uma linha e o comando \t dá espaço de uma identação
 name = "\n\tjoao "
 print(name)
 print(name.strip())
