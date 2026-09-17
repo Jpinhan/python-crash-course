b1da36bd86c46c63de8fdd00a56ac7c0de443482
João <joaopinhan@gmail.com>
2026-09-17 14:12:09 -0300

    Adiciona exercicio 3.4 do capitulo 3

---
+++ introducao-as-listas/3.4.py
@@ -0,0 +1,4 @@
+convidados = ["mãe", "pai", "irmão"]
+print(convidados[0].title() + ", Vamos jantar?")
+print(convidados[1].title() + ", Vamos jantar?")
+print(convidados[2].title() + ", Vamos jantar?")
