ceca454b596c68f21b0c8c1cb598df87bb9d1e4d
João <joaopinhan@gmail.com>
2026-09-17 13:55:49 -0300

    Adiciona o exercicio 3.3 do capitulo 3

---
+++ introducao-as-listas/3.3.py
@@ -0,0 +1,3 @@
+motorcyles = ["Honda CBR 1000RR-R Fireblade", "Yamaha YZF-R3"]
+print("A moto "+motorcyles[0] +" possui 1000cc e quatro cilindros, com potência de 217 cavalos.")
+print("A moto "+motorcyles[1] +" possui 321cc e dois cilindros, com potência de 42 cavalos.")
