2d87ea925225bb27a311209a444ce80ede790055
João <joaopinhan@gmail.com>
2026-09-18 19:38:33 -0300

    Adiciona exercicio 3.8 do capitulo 3

---
+++ introducao-as-listas/3.8.py
@@ -0,0 +1,13 @@
+viagem = ["Cataratas do Iguaçu", "Fernando de Noronha", "Transatlantico", "Itália", "Roma"]
+print(viagem)
+# Utilize sorted() para exibir sua lista em ordem alfabética, sem modificar a lista propriamente dita.
+print(sorted(viagem))
+print(viagem)
+viagem.reverse()
+print(viagem)
+viagem.reverse()
+print(viagem)
+viagem.sort()
+print(viagem)
+viagem.sort(reverse = True)
+print(viagem)
