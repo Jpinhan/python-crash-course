477c4ba1ffd207fe97840c638755d5284241eeef
João <joaopinhan@gmail.com>
2026-09-18 20:35:44 -0300

    Adiciona exercicio 4.1 do capitulo 4

---
+++ trabalhando-com-listas/4.1.py
@@ -0,0 +1,6 @@
+pizzas = ["mussarela", "Calabresa", "frango com catupiry", "Marguerita"]
+for pizza in pizzas:
+    print("Pizza de "+pizza.title() + " é boa!")
+    print("Mas eu prefiro de Brigadeiro!\n")
+
+print("Infelizmente eu estou de dieta e faz meses que eu não como pizza")
