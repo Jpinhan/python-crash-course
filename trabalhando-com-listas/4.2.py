bb767471a80eb48a580920378f18b00bd921c5df
João <joaopinhan@gmail.com>
2026-09-18 21:02:48 -0300

    Adiciona exercicio 4.2 do capitulo 4

---
+++ trabalhando-com-listas/4.2.py
@@ -0,0 +1,8 @@
+animais = ["Capivara", "Carcará", "Urubu", "Teiú"]
+for animal  in animais:
+    print(animal)
+print(animais[0]+ " é o maior mamífero roedor do mundo!")
+print(animais[1]+ " muda a cor do rosto conforme o nível de estresse")
+print(animais[2]+ " tem o suco gástrico dez vezes mais forte que o dos seres humanos")
+print(animais[3]+ " é o maior lagarto do Brasil e pode pesar até 5kg")
+print('\nUma coisa em comum sobre esses animais é que eu vejo todos semanalmente perto do minha casa')
